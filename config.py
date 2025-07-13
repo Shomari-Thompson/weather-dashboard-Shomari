f38d5a00c7787700be6c0a9d8eca583cf6aed3dc
Shomari-Thompson <shomarit1@gmail.com>
2025-07-12 20:12:13 -0400

    Secured API key

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
-API_KEY = "53e2a44212d3e20346601d266a409732"
-BASE_URL = "https://api.openweathermap.org/data/2.5/weather"
+# config.py
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
+
+API_KEY = os.getenv("API_KEY")
+BASE_URL = os.getenv("BASE_URL")
 DEFAULT_CITY = "New York"
